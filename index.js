d1b71f1e773800499628563c8da22d6f045dd1fd
maxferol <ferol.max@gmail.com>
2026-03-03 19:20:11 +0500

    sort

--- index.js
+++ index.js
@@ -62,6 +62,13 @@ function processCommand(command) {
             }
             break;
 
+        case 'sort':
+            processSort(command_date);
+            for (let comment of res){
+                console.log(comment);
+            }
+            break;
+
         default:
             console.log('wrong command');
             break;
@@ -69,14 +76,59 @@ function processCommand(command) {
 }
 
 
-function processSort(sortBy){
-    switch (sortby) {
-        case "importance":
+function processSort(sortBy) {
+    switch (sortBy) {
+        case 'importance':
+            res.sort((a, b) => compareByImportance(a, b));
+            break;
 
-        case "user":
+        case 'user':
+            res.sort((a, b) => compareByUser(a, b));
+            break;
 
-        case "date":
+        case 'date':
+            res.sort((a, b) => compareByDate(a, b));
+            break;
     }
 }
+
+function compareByImportance(a, b){
+    let a_count = a.slice(a.indexOf('!'), a.lastIndexOf('!') + 1).length;
+    let b_count = b.slice(b.indexOf('!'), b.lastIndexOf('!') + 1).length;
+    if (a_count < b_count)  return 1;
+    else if (a_count > b_count)  return -1;
+    return 0;
+}
+
+
+function compareByUser(a, b){
+    aUser = a.slice(5, a.indexOf(';')).toLowerCase();
+    bUser = b.slice(5, b.indexOf(';')).toLowerCase();
+
+    if (aUser < bUser)
+        return -1;
+    else if (aUser > bUser)
+        return 1;
+    return 0;
+}
+
+function compareByDate(a, b){
+    let a_data = a.slice(a.indexOf(';') + 1, a.lastIndexOf(';')).replace(' ', '');
+    let b_data = b.slice(b.indexOf(';') + 1, b.lastIndexOf(';')).replace(' ', '');
+    let [a_year, a_month, a_day] = a_data.split('-');
+    let [b_year, b_month, b_day] = b_data.split('-');
+    if (b_year < a_year) return 1;
+    else if (b_year > a_year) return -1;
+    else{
+        if (b_month < a_month) return 1;
+        else if (b_month > a_month) return -1;
+        else{
+            if (b_day < a_day) return 1;
+            else if (b_day > a_day) return -1;
+            else return 0;
+        }
+    }
+}
+
 
 // TODO you can do it!
