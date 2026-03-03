b1545462f38d99ab02e62fb4bdf3f7e7a6e34774
maxferol <ferol.max@gmail.com>
2026-03-03 18:28:59 +0500

    parsing

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
+const commentStart = "// ";
 const {getAllFilePathsWithExtension, readFile} = require('./fileSystem');
 const {readLine} = require('./console');
 
 const files = getFiles();
 
+let todoComments = [];
+getAllTodoComments();
+for (let comment of todoComments){
+    console.log(comment);
+}
+
 console.log('Please, write your command!');
 readLine(processCommand);
+
 
 function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
     return filePaths.map(path => readFile(path));
 }
+
+function getTodoComments(file) {
+    file = file.substring(file.indexOf("\n") + 1);
+    let lines = file.slice(file.indexOf(commentStart)).split(commentStart);
+    lines = lines.slice(1,);
+    for (let line of lines){
+        let endIndex = line.indexOf('\n');
+        line = line.substring(0, endIndex);
+        todoComments.push(line);
+    }
+}
+
+function getAllTodoComments() {
+    for (const file of files){
+        getTodoComments(file);
+    }
+}
+
 
 function processCommand(command) {
     switch (command) {
